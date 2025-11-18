b7e14730b2e740d601a886e9f6e3ea7108c47980
Igor <zeroigorsr@gmail.com>
2025-11-18 15:39:26 -0300

    mais exercicios

---
+++ iniciante/1008.py
@@ -0,0 +1,6 @@
+numero = int(input())
+horas = int(input())
+salario = float(input())
+
+print(f"NUMBER = {numero}")
+print(f"SALARY = U$ {horas*salario:.2f}")
---
+++ iniciante/1009.py
@@ -0,0 +1,7 @@
+nome = input()
+salario = float(input())
+total_vendas = float(input())
+
+total = salario + (total_vendas * 0.15)
+
+print(f"TOTAL = R$ {total:.2f}")
