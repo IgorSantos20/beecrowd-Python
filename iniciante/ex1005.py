80dcb159d4ee445fef097b6ec03b6dbeb36f0109
Igor <zeroigorsr@gmail.com>
2025-11-18 15:08:36 -0300

    5 primeiros exercicios

---
+++ iniciante/ex1005.py
@@ -0,0 +1,7 @@
+a = float(input())
+b = float(input())
+
+pesoA = 3.5
+pesoB = 7.5
+
+print(f"MEDIA = {(a*pesoA + b*pesoB) / 11:.5f}")
---
+++ iniciante/ex1006.py
@@ -0,0 +1,9 @@
+notaA = float(input())
+notaB = float(input())
+notaC = float(input())
+
+pesoA = 2
+pesoB = 3
+pesoC = 5
+
+print(f"MEDIA = {(notaA*pesoA + notaB*pesoB + notaC*pesoC) / 10:.1f}")
